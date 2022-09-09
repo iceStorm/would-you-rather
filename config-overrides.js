1d6dcdf476e89cb0debdc17d910004f500f52541
Anh Tuan <9Tuan_Nguyen@Singaporeair.com.sg>
2022-09-09 12:34:50 +0700

    remove react-app-reqired

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* config-overrides.js */
-
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-
-    // true: use postcss.config.js'
-
-    return config
-}
